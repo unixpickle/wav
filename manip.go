3ca6cc8d8070e98b67d40ed26e1887b0446fbccc
Alex Nichol <unixpickle@gmail.com>
2014-12-30 12:54:55 -0500

    added Append() method

--- manip.go
+++ manip.go
@@ -2,7 +2,23 @@ package wav
 
 import "time"
 
-func Crop(s Sound, start time.Duration, end time.Duration) {
+// Append appends sounds to a sound.
+// If the various sounds have different numbers of channels, channels will be
+// added or removed as needed.
+// This works best if all the sounds have the same sample rate. Otherwise,
+// the result will have some faster and some slower parts.
+func Append(s1 Sound, sounds ...Sound) {
+	for _, s := range sounds {
+		if s.Channels() != s1.Channels() {
+			diffChannelAppend(s1, s)
+			continue
+		}
+		s1.SetSamples(append(s1.Samples(), s.Samples()...))
+	}
+}
+
+// Crop isolates a time segment in a sound.
+func Crop(s Sound, start, end time.Duration) {
 	// Cannot crop an empty sound.
 	sampleCount := len(s.Samples())
 	if sampleCount == 0 {
@@ -32,4 +48,22 @@ func Crop(s Sound, start time.Duration, end time.Duration) {
 
 	// Perform crop
 	s.SetSamples(s.Samples()[startIdx:endIdx])
+}
+
+func diffChannelAppend(s1, s2 Sound) {
+	if s2.Channels() > s1.Channels() {
+		for _, x := range s2.Samples() {
+			cut := x[0:s1.Channels()]
+			s1.SetSamples(append(s1.Samples(), cut))
+		}
+	} else {
+		for _, x := range s2.Samples() {
+			bigger := make([]Sample, s1.Channels())
+			copy(bigger, x)
+			for i := len(x); i < len(bigger); i++ {
+				bigger[i] = bigger[0]
+			}
+			s1.SetSamples(append(s1.Samples(), bigger))
+		}
+	}
 }
